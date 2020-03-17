e621972762aee63ae89bd0e798f44ce33d18462e
Eyayaw <eyuecon@gmail.com>
2020-03-17 20:37:27 +0100

    m11 and m12

--- src/Project_wrangling.R
+++ src/Project_wrangling.R
@@ -50,10 +50,10 @@ tidy_it <- function(database) {
   for (day in seq_along(database)) {
        for (article in seq_along(database[[day]])) {
       
-      out[[day]][[article]] <- set_names(x = database[[day]][[article]], 
-                                         nm = create_names(x = database[[day]][[article]], 
-                                                           name_prefix = "var")) %>% 
-        flatten() %>% as_tibble(x = ., .name_repair = "unique") %>% 
+      out[[day]][[article]] <- database[[day]][[article]] %>% 
+        set_names(create_names(x = ., name_prefix = "var")) %>% 
+        flatten() %>% 
+        as_tibble(x = ., .name_repair = "unique") %>% 
         mutate(var_4 = paste(select(., -contains("var")), collapse = "||")) %>% 
         select(contains("var"))
     }
@@ -72,25 +72,22 @@ tidy_it <- function(database) {
   
   df$Day_of_the_month <- Day_of_the_month
   
-  df <- df %>% mutate(Date = str_extract(Date, "\\d{4}.\\d{2}.\\d{2}")) %>% 
+  df <- df %>% 
+    mutate(Date = str_extract(Date, "\\d{4}.\\d{2}.\\d{2}")) %>% 
     mutate(Date = lubridate::ymd(str_replace_all(Date, "[^0-9]",  "-")))
-  
   return(df)
 }
 
-read_and_tidy <- function(x, fun) {
-  df <- readRDS(x)
-  df <- fun(df)
-  return(df)
-}
+
+# even faster
 
 tidy_it2 <- function(database) {
-  df <- map_df(database, function(.x) {
+  df <- map_df(database, function(x) {
     tibble(
-      Date = map_chr(.x, 1), 
-      subtitle = map_chr(.x, 2), 
-      title = map_chr(.x, 3), 
-      content = map_chr(.x, function(x) x[[4]] %>% paste0(collapse = "||")))
+      Date = map_chr(x, 1), 
+      subtitle = map_chr(x, 2), 
+      title = map_chr(x, 3), 
+      content = map_chr(x, function(xx) paste0(xx[[4]], collapse = "||")))
   })
   # parse Date from the Date column of each tibble.
   df <- mutate(.data = df, 
@@ -103,14 +100,21 @@ tidy_it2 <- function(database) {
 
 # convert lists into tidy tibbles -----------------------------------------
 
+read_and_tidy <- function(x, fun) {
+  df <- readRDS(x)
+  df <- fun(df)
+  return(df)
+}
 paths <- fs::dir_ls("data/")
 names(paths) <- str_sub(paths, start = -16, end = -5)
 
 # using function tidy_it
-df <- suppressMessages(purrr::map(paths[-c(11, 12)], read_and_tidy, fun = tidy_it))
+df <- suppressMessages(
+  purrr::map(paths, read_and_tidy, fun = tidy_it)
+  )
 
 # using function tidy_it2
-df_2 <- purrr::map(paths[-c(11, 12)], read_and_tidy, fun = tidy_it2)
+df_2 <- purrr::map(paths, read_and_tidy, fun = tidy_it2)
 
 
 ##### ----- write files into .csv or .rds
@@ -119,17 +123,17 @@ df_2 <- purrr::map(paths[-c(11, 12)], read_and_tidy, fun = tidy_it2)
 # fs::dir_create('output/tidy_it')
 # fs::dir_create('output/tidy_it2')
 
-walk2(.x = df, 
-      .y = names(df), 
+walk2(.x = df,
+      .y = names(df),
       ~write_rds(.x, paste0('output/tidy_it/', .y, '.rds')))
 
-walk2(.x = df_2, 
-      .y = names(df_2), 
+walk2(.x = df_2,
+      .y = names(df_2),
       ~write_rds(.x, paste0('output/tidy_it2/', .y, '.rds')))
 
 # all in one
 
-news_article_2020 <- df_2 %>% bind_rows()
+news_article_2020 <- bind_rows(df_2)
 
 write_rds(news_article_2020, 'output/news_article_2020.rds')
 
