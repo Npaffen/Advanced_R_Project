9d6c365a37e8225aaa770c088a2e043c53da1f56
Npaffen <nils.paffen@wopic.de>
2020-03-23 17:11:23 +0100

    Scraper with captcha solver

--- src/Scaper.R
+++ src/Scaper.R
@@ -5,14 +5,15 @@ library(lubridate)
 library(httr)
 library(xml2)
 library(stringr)
-
+library(magick)
+library(rlist)
 #### PLEASE USE THIS GUIDE TO INSTALL DOCKER https://rpubs.com/johndharrison/RSelenium-Docker
 
 Sys.setlocale("LC_TIME", "C")
 
 all.nodes <- c(".sha_right ", " .author", ".subtitle", 
                ".sha_left span", " #FontZoom", " .title")
-
+captcha_tester <- read_html("captcha.html")
 # date <- (today()-1)-0:time_length(interval(ymd(str_c((year((today()-1))-5), str_c(0,month((today()-1))), day((today()-1)) , sep = "-" )),
 #                                            ymd((today()-1))) , unit = "day")
 
@@ -21,7 +22,7 @@ all.nodes <- c(".sha_right ", " .author", ".subtitle",
 # avoid overwriting it, so `dates` would be a good name.
 
 dates <- seq(from = as.Date('2015-01-01'),
-             to = as.Date('2019-01-01'),
+             to = as.Date('2020-03-23'),
              by = 1)
 # the result is already is a character vector.
 
@@ -65,25 +66,45 @@ f_articles_url <- function(paper_length, dates) {
   
 }
 
-f_content <- function(url_articles) {
-  Sys.sleep(runif(1,5,10))
-  remDr$navigate(
-    str_c(
-      "http://data.people.com.cn.s894ibwr0870.erf.sbb.spk-berlin.de/rmrb",
-      url_articles,
-      sep = "/"
-    )
-  )
+f_captcha_content <- function(){
+ 
+  remDr$screenshot(file = "captcha.png") #screenshot of the full site with captcha
+  magick::image_crop(image_read("captcha.png"),geometry_area(97,38.5,480,290) ) %>%
+    image_write( "captcha.png", format = "png") #crop the captcha
+    
   
   
-  #captcha <- read_html(remDr$getPageSource()[[1]])
-  #download_html(remDr$navigate(""), file = "captcha.hmtl")
   
-  if (read_html(remDr$getPageSource()[[1]])%>%
-      html_nodes() %>% html_text()  == captcha_text) message(str_c("Captcha Code after", Sys.time() - start_time, sep = " " ))
-  else
+  cap_POST <-  httr::POST(url = "http://2captcha.com/in.php?key=d3ce30748e45dc73365f4e327acaebee&method=post",
+                          encode = "multipart",
+                          body = list(file = upload_file(path = "captcha.png"))) #send the captcha to the api
+  captcha_ID <- content(cap_POST) %>%
+    xml_child() %>%
+    xml_text() %>%
+    gsub('[^0-9]+',
+         replacement = "",
+         x =.) #catch the ticket ID
   
+  Sys.sleep(20L) #wait untill solving
   
+  if(httr::GET(url = str_c("https://2captcha.com/res.php?key=d3ce30748e45dc73365f4e327acaebee&action=get&id=",
+                           captcha_ID,
+                           sep = ""))%>% 
+     content() == "CAPCHA_NT_READY") #check if the captcha is solved
+    Sys.sleep(5L)# add extra time if not
+  
+    
+    captcha_key <- httr::GET(url = str_c("https://2captcha.com/res.php?key=d3ce30748e45dc73365f4e327acaebee&action=get&id=",
+                                         captcha_ID,
+                                         sep = "")) %>% 
+    content() %>%
+    gsub(x = .,
+         pattern ="[OK|]",
+         replacement = "")# grab the captcha code
+  
+  remDr$findElement("css selector", "#validateCode")$sendKeysToElement(list(captcha_key, key = "enter")) # post it to the form
+  
+
   map(
     all.nodes,
     ~ read_html(remDr$getPageSource()[[1]]) %>%
@@ -99,103 +120,84 @@ f_content <- function(url_articles) {
         pattern = "(\\n\\t{3,})",
         replacement = ""
       )
+  ) # grab the article content
+  }
+
+f_content <- function(url_articles) {
+  remDr$navigate(
+    str_c(
+      "http://data.people.com.cn.s894ibwr0870.erf.sbb.spk-berlin.de/rmrb",
+      url_articles,
+      sep = "/"
+    )
   )
+  
+  
+
+  
+  if (read_html(remDr$getPageSource()[[1]]) %>% xml_child( 2)%>% xml_child( 2) %>% 
+html_text() == html_text(xml_child(xml_child(captcha_test, 2), 2)))#check if the archive requests a captcha
+  f_captcha() #solve the captcha if necessary
+ 
+  
+  
+  
+  else if (read_html(remDr$getPageSource()[[1]]) %>% xml_child( 2)%>% xml_child( 2) %>% 
+        html_text() == html_text(xml_child(xml_child(captcha_test, 2), 2))) # check if the captcha was solved correctly
+      f_captcha() # solve again if not true
+  
+  else 
+  map(
+    all.nodes,
+    ~ read_html(remDr$getPageSource()[[1]]) %>%
+      html_nodes(.x) %>%
+      html_text() %>%
+      gsub(
+        x = . ,
+        pattern = "\\s{10,}",
+        replacement = ""
+      ) %>%
+      gsub(
+        x = .,
+        pattern = "(\\n\\t{3,})",
+        replacement = ""
+      )
+  ) #grab the article content
   
 }
 
 
 f_scraper <- function(dates) {
-  start_time <- Sys.time()
-  #  remDr$navigate(
-#    str_c(
-#      "http://data.people.com.cn.s894ibwr0870.erf.sbb.spk-berlin.de/rmrb",
-#      dates,
-#      1,
-#      sep = "/"
-#    )
-#  )
-  paper_length <- 1:2 # read_html(remDr$getPageSource()[[1]]) %>% 
-#    html_nodes("#banci_btn > div > div > ul") %>%
-#    xml_length() %>%
-#    1:.
+
+  paper_length <- 1:2 # Only page 1 and 2 of each newspaper will be analyzed
+
   
-  url_articles <- map(paper_length,  ~ f_articles_url(.x, dates[1]))
+  url_articles <- map(paper_length,  ~ f_articles_url(.x, dates)) #grab the article adresses
   
-  map(flatten(url_articles), ~ f_content(.x))
-}
-
-data
-database_m1 <- map(dates[(length(dates)-30):length(dates)],  ~ f_scraper(.x))
-database_m2 <- map(dates[(length(dates)-13):(length(dates)-7)],  ~ f_scraper(.x))
-
-
-#######################################NEW SCRAPER########
-dates <- seq(from = as.Date('2019-01-01'),
-            to = as.Date('2020-01-01'),
-            by = 1)
-
-monyear <- dates %>% sub(pattern = "-\\d{2}$",
-                         "",
-                         x = .) 
-
-sday <- dates %>% sub(pattern = "\\d{4}-\\d{2}-" ,
-                      "",
-                      x = .)
-
-dates <- map2(monyear, sday, ~str_c(.x, .y, sep = "/")) %>% unlist()
-
-
-#Zeitung 1 http://paper.people.com.cn/rmrb/html/2020-01/30/nw.D110000renmrb_20200130_3-02.htm nodes : .lai , h1, p
-
-f_url_articles <- function(dates, paper_length){
-  
-  read_html(str_c("http://paper.people.com.cn/rmrb/html/", 
-                  dates,
-                  "/nbs.D110000renmrb_0",
-                  paper_length, 
-                  ".htm", sep = "")) %>%
-    html_nodes( "#titleList a") %>%
-    html_attr("href") %>%
-    map(~str_c(dates, .x, sep = "/"))
-}
-all_nodes <- c(".lai "," h2"," h1", "#ozoom p", ".list_l .l_t")
-f_content <- function(url_articles){
-  article <- read_html(str_c("http://paper.people.com.cn/rmrb/html/",
-                             url_articles, 
-                             sep = "/"))
-  map(all_nodes, ~html_nodes(article, .x) %>%
-        html_text()) 
-    
-  
-}
-
-f_scraper <- function(dates){
-  paper_length <- 1:2 # since we only want page 1 & 2
-  
-  url_articles <- map(paper_length, ~f_url_articles(dates = dates, paper_length = .x))
-  
-  
-  map(unlist(url_articles), ~f_content(.x))
+  map(flatten(url_articles), ~ f_content(.x)) #grab the content
 }
 
 
+#shell("git.lnk config --global user.email \"nils.paffen@wopic.de\"")
+#shell("git.lnk config --global user.name \"npaffen\"")
 
+archive_partly <- dates <- seq(from = as.Date('2019-01-01'),
+                               to = as.Date('2020-03-23'),
+                               by = 1)
 
-
-
-#DO NOT EXECUTE WILL BE DELETED LATER
-
-shell("git.lnk config --global user.email \"nils.paffen@wopic.de\"")
-shell("git.lnk config --global user.name \"npaffen\"")
-soda <- seq(from = length(dates)-29, to = length(dates), by =29)
-j = 10
-for (i in seq_along(soda)){
+time_span_archive_monthly <- seq(from = 1,
+                                 to = length(dates)-length(archive_partly),
+                                 by = 30)
+j = 12
+for (i in seq_along(sort(time_span_archive_monthly, decreasing = T))){
   j = j + 1
   
-  assign(str_c("database","_m", j, sep = ), map(dates[(length(dates)-i-29):(length(dates)-i)],  ~ f_scraper(.x)))
-  
+  assign(str_c("database","_m", j, sep = ""),
+         map(dates[(length(dates)-i-30):(length(dates)-i)],
+             ~ f_scraper(.x)))
+  list.save(x =str_c("database","_m", j, sep = ), str_c("database","_m", j,".rds", sep = ""))
 }
-list.save("database","_m", j,".rds", sep = )
+
   remDr$screenshot
 
 
