f8270a5919956d2dc914a7ef060ecedd184e52d5
Eyayaw <eyuecon@gmail.com>
2020-03-25 13:37:52 +0100

    changed how `generate_article_url()` behaves.

--- src/scrape_article.R
+++ src/scrape_article.R
@@ -1,36 +1,54 @@
+
+# load packages -------------------------------------------------
+
+library(lubridate)
+library(rvest)
+library(tidyverse)
+library(glue)
+
+
 ## This function creates a url for a "edition/version 01" article published
 # on a specific date and for columns on it.
 
 generate_article_url <- function(date, edition = "01") {
-  date <- lubridate::ymd(date)
-  yyyy <- lubridate::year(date)
-  mm <- lubridate::month(date)
+  date <- ymd(date)
+  yyyy <- year(date)
+  mm <- month(date)
   mm <- if_else(mm < 10, paste0(0, mm), as.character(mm))
-  dd <- lubridate::day(date)
+  dd <- day(date)
   dd <- if_else(dd < 10, paste0(0, dd), as.character(dd))
 
-  initial <- glue::glue("http://paper.people.com.cn/rmrb/html/{yyyy}-{mm}/{dd}/")
+  initial <- glue("http://paper.people.com.cn/rmrb/html/{yyyy}-{mm}/{dd}")
 
   # article link
-  article_url <- glue::glue("{initial}nbs.D110000renmrb_{edition}.htm")
+  article_url <- glue("{initial}/nbs.D110000renmrb_{edition}.htm")
 
 
-  # counts the number of columns or sections in the article.
+  # Using the following code (which is bound to cols*), we found out that
+  # the maximum number of columns in a single article is 9.
+  # We do not have to scrape just to get the number of column counts
+  # for each article, we default the number of columns or sections in any
+  # article to 9. Anyway, the use of safely() in the data getter function
+  # (see `get_article_data()` below) captures the error if the actual
+  # column counts are below or above 9.
 
-  cols <- read_html(article_url) %>%
-    html_nodes('a[href*="nw.D110000renmrb"]') %>%
-    html_attr("href") %>%
-    unique()
+  # checker -------------------------------------------------------
+  # cols <- read_html(article_url) %>%
+  #           html_nodes('a[href*="nw.D110000renmrb"]') %>%
+  #           html_attr("href") %>%
+  #           unique() # *
+  # checker -------------------------------------------------------
 
-  # cols_num <- seq(1, length(cols))
-
+  max_cols <- 9
 
   yyyymmdd <- paste0(yyyy, mm, dd)
 
+  # suffix for columns url
+  cols <- glue("nw.D110000renmrb_{yyyymmdd}_{1:max_cols}-{edition}.htm")
 
-  cols_url <- glue::glue("{initial}{cols}")
+  cols_url <- glue("{initial}/{cols}")
 
-  list(
+  tibble(
     article_url = article_url,
     cols_url = cols_url,
     date = date,
@@ -89,37 +107,13 @@ safely_slowly_extract <- safely(
 )
 
 
+# create a function that extractes the article text data and ----
+# other additional infor.
 
-
-# create vectors of dates.
-
-date_vec_2020 <- seq(as.Date("2020-01-01"), lubridate::today(), by = 1)
-
-date_vec_2019 <- seq(as.Date("2019-01-01"), lubridate::ymd("2020-01-01") - 1, by = 1)
-
-########## for 2020
-article_urls_2020 <- map(date_vec_2020, generate_article_url)
-
-article_urls_2020 <- setNames(article_urls_2020, date_vec_2020)
-
-
-article_urls_2020 <- map_df(article_urls_2020, as_tibble)
-
-# saveRDS(article_urls_2020, 'data/article_urls_2020.RDS')
-
-########## for 2019
-article_urls_2019 <- map(date_vec_2019, generate_article_url)
-
-article_urls_2019 <- setNames(article_urls_2019, date_vec_2019)
-
-
-article_urls_2019 <- map_df(article_urls_2019, as_tibble)
-
-# saveRDS(article_urls_2019, 'data/article_urls_2019.RDS')
-
-count <- 0
 
 get_article_data <- function(article_urls) {
+  count <- 0
+
   article_data <- map(article_urls$cols_url, function(x) {
     count <<- count + 1
 
@@ -141,17 +135,53 @@ get_article_data <- function(article_urls) {
 
 
   article_data_ok <- bind_rows(article_data$result[is_ok], .id = "id")
+  article_data_notok <- article_data$error[!is_ok]
 
   rm(article_data)
   rm(is_ok)
 
-  article_data_ok
+  article_data_ok %>%
+    mutate(
+      date = ymd(str_extract(id, "\\d{8}")),
+      column_num = str_extract(id, "(_\\d-)"),
+      column_num = str_extract(column_num, "\\d")
+    )
+
+  list(
+    article_data_ok = article_data_ok,
+    article_data_notok = article_data_notok
+  )
 }
 
+# create vectors of dates.
 
-article_data_ok_2020 <- get_article_data(article_urls_2020)
-article_data_ok_2019 <- get_article_data(article_urls_2019)
+date_vec_2020 <- seq(ymd("2020-01-01"), today(), by = 1)
 
+date_vec_2019 <- seq(ymd("2019-01-01"), ymd("2020-01-01") - 1, by = 1)
+
+# For 2020 ---------------------------------------------------
+article_urls_2020 <- map_df(date_vec_2020, generate_article_url)
+
+
+# saveRDS(article_urls_2020, 'data/article_urls_2020.RDS')
+
+# For 2019 ---------------------------------------------------
+
+article_urls_2019 <- map_df(date_vec_2019, generate_article_url)
+
+# saveRDS(article_urls_2019, 'data/article_urls_2019.RDS')
+
+
+
+article_data_2020 <- get_article_data(article_urls_2020)
+
+# successful ones
+article_data_ok_2020 <- article_data_2020$article_data_ok
+
+article_data_2019 <- get_article_data(article_urls_2019)
+
+# successful ones
+article_data_ok_2019 <- article_data_2019$article_data_ok
 
 # saveRDS(article_data_ok_2020, 'data/article_data_ok_2020.rds')
 # saveRDS(article_data_ok_2019, 'data/article_data_ok_2019.rds')
