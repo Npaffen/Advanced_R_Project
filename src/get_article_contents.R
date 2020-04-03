ae6ca6c315d58aa49feba665ca392dd2d1e205d4
Eyayaw <eyuecon@gmail.com>
2020-04-03 20:29:28 +0200

    fixed issues raised by @David

--- src/get_article_contents.R
+++ src/get_article_contents.R
@@ -2,31 +2,31 @@
 
 extract_content <- function(column_url) {
   page <- read_html(column_url)
-
+  
   get_h1 <- compose(html_text,
-    partial(html_nodes, css = "h1"),
-    partial(html_nodes, css = ".text_c"),
-    .dir = "backward"
+                    partial(html_nodes, css = "h1"),
+                    partial(html_nodes, css = ".text_c"),
+                    .dir = "backward"
   )
-
+  
   get_h3 <- compose(html_text,
-    partial(html_nodes, css = "h3"),
-    partial(html_nodes, css = ".text_c"),
-    .dir = "backward"
+                    partial(html_nodes, css = "h3"),
+                    partial(html_nodes, css = ".text_c"),
+                    .dir = "backward"
   )
-
+  
   get_paragraph <- compose(html_text,
-    partial(html_nodes, css = "p"),
-    partial(html_nodes, css = ".c_c"),
-    .dir = "backward"
+                           partial(html_nodes, css = "p"),
+                           partial(html_nodes, css = ".c_c"),
+                           .dir = "backward"
   )
-
+  
   df <- list(
     title = get_h1(page),
     subtitle = get_h3(page),
     content = get_paragraph(page)
   )
-
+  
   df <- tibble(
     title = df$title,
     subtitle = df$subtitle,
@@ -49,7 +49,7 @@ extract_content <- function(column_url) {
 
 get_article_data <- function(article_urls) {
   len <- length(article_urls$column_url)
-
+  
   dat <- map(article_urls$column_url, function(x) {
     len <<- len - 1
     if (len %% 10 == 0 || len < 10) {
@@ -63,37 +63,33 @@ get_article_data <- function(article_urls) {
     )(x)
     # so that there is a short pause between consecutive requests.
   })
-
-
+  
+  
   names(dat) <- article_urls$cols
-
+  
   dat <- transpose(dat)
-
-
+  
+  
   is_ok <- dat$error %>% map_lgl(is_null)
-
-
+  
+  
   dat_ok <- suppressWarnings(bind_rows(dat$result[is_ok], .id = "id"))
   dat_notok <- dat$error[!is_ok]
-
+  
   dat_ok <- dat_ok %>%
     mutate(
       date = ymd(str_extract(id, "\\d{8}")),
       column_num = str_extract(id, "(_\\d-)"),
       column_num = str_extract(column_num, "\\d"),
-<<<<<<< HEAD
       id = str_replace(str_extract(id, "_[\\d_-]+"), "-", "_"),
       page_num = str_extract(id, "\\d{2}$")
     ) %>%
     select(
       id, page_num, date, column_num, title, subtitle,
       content, everything()
-=======
-      id = str_replace(str_extract(id, "_[\\d_-]+"), "-", "_")
->>>>>>> parent of e84f999... the second page articles scraped and wrote the data_download function
     )
-
- dat_ok
+  
+  dat_ok
 }
 
 
--- src/update_article.R
+++ src/update_article.R
@@ -2,45 +2,43 @@ library(tidyverse)
 library(lubridate)
 library(glue)
 # Updating data as new articles unfold every new day
-# page_num refers to the page that should be updated
-# "01" is the title page, "02" the second page, etc.
 
 update_article_data <- function(page_num,
                                 write_to_disk = FALSE) {
   stopifnot(page_num %in% c("01", "02"))
-
+  
   path <- paste0("data/article_data_2020_page_", page_num, ".rds")
-
+  
   nms <- str_sub(path, 6, -5)
-
+  
   names(path) <- nms
-
+  
   df <- map(path, read_rds)
-
+  
   last_updated <- map(df, ~ max(.x[["date"]]))
-
+  
   # diff <- map(last_updated, ~ today() - .x)
-
+  
   for (j in seq_along(last_updated)) {
     message(glue("Page {page_num[[j]]}, last updated on {last_updated[[j]]}.\n"))
   }
-
+  
   last_updated <- keep(last_updated, ~ .x < today())
-
+  
   if (!is_empty(last_updated)) {
-    dates_to_be_updated <- map( ## this throws "non-numeric argument for binary operator
+    dates_to_be_updated <- map(
       last_updated,
       ~ seq.Date(.x + 1, today(), by = 1)
     )
-
-    source("src/scrape_article.R") ## dirty git merge in that file :( 
-
+    
+    source("src/scrape_article.R")
+    
     dat <- map2(
       page_num, dates_to_be_updated,
       ~ scrape_article(.x, .y)
     )
     names(dat) <- nms
-
+    
     for (nm in nms) {
       df[[nm]] <- bind_rows(df[[nm]], dat[[nm]])
     }
@@ -48,11 +46,11 @@ update_article_data <- function(page_num,
     if (write_to_disk) {
       walk2(df, names(df), 
             ~ saveRDS(.x, paste0("data/", .y, ".rds")))
-      }
-
+    }
+    
     pwalk(list(df, dat, names(df)), function(.x, .y, .z) {
       message("successfully updated!!")
-
+      
       cat(
         "nrows of ", "<<", .z, ">>: ",
         "<<", (nrow(.x) - nrow(.y)), ">> ",
