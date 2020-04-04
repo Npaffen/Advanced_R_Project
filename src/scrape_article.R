28ee21391fa6187039fbbe4296603c3225d4417b
Eyayaw <eyuecon@gmail.com>
2020-04-04 15:13:32 +0200

    touched scrape_article.r

--- src/scrape_article.R
+++ src/scrape_article.R
@@ -7,6 +7,7 @@ library(glue)
 # if dates is not supplied, then ... passes args to the make_dates()
 # function, which are [ year, month, from_day, to_day, all_dates (logical),
 # respectively ]. Please refer to `make_dates.R`.
+
 scrape_article <- function(page_num, dates = NULL, ...) {
   source("src/make_dates.R") # sources make_dates()
   source("src/generate_article_urls.R") # sources generate_urls()
@@ -17,14 +18,11 @@ scrape_article <- function(page_num, dates = NULL, ...) {
   }
 
   # Article urls for the given year -----------------------------
-  # ================================= -----------------------------
-  article_urls <- suppressWarnings(map_df(dates, ~ generate_urls(.x, page_num)))
+  # ================================= ---------------------------
+  article_urls <- suppressWarnings(
+    map_df(dates, ~ generate_urls(.x, page_num))
+    )
   # Download content ----------------------------------------------
   source("src/get_article_contents.R")
-
-  article_data <- get_article_data(article_urls)
-
-  # returns only the successful ones
-  # final output of the function
-  article_data
+   get_article_data(article_urls)
 }
