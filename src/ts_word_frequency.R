36939d540f14a96d264ea20a8f1bd2e3556bc098
Npaffen <57102530+Npaffen@users.noreply.github.com>
2020-04-07 15:22:33 +0200

    ts tidy

--- src/ts_word_frequency.R
+++ src/ts_word_frequency.R
@@ -1,17 +1,17 @@
-# page_num is the number of newspaper page either 1 or 2, start and end_date 
+  e# page_num is the number of newspaper page either 1 or 2, start and end_date 
 #define the time-span for the anlysis, eng_word is the english word a user is looking for,
 #econ_data can either be "NASDAQ_CNY" or "dollar_yuan_exch" to plot the eng_word frequency against
 # this economic indicator
 ts_word_frequency <- function(page_num = 1, start_date = as.Date("2019-01-01"),
                               end_date = today(), eng_word = "outbreak", econ_data = "NASDAQ_CNY") {
-
+  
   ## please add a description, what is eng_word, etc.
   ## adding a table of contents also makes sense at this length
   
 library(dplyr)
 library(purrr)
 library(stringr)
-library(tidytext)
+library(tidytextxt)
 library(readr)
 library(stopwords)
 library(tidyr)
@@ -22,101 +22,14 @@ library(Quandl)
 library(fredr)
 library(ggplot2)  
   
-#### function to convert monthly econ data to daily econ data
-monthly_to_daily  <- function(ts_monthly) {
-  df.xts <- xts(ts_monthly$Value,order.by = ts_monthly$Date)
-  ts_monthly_daily <- na.locf(merge(df.xts, 
-                                    foo=zoo(NA,
-                                            order.by=seq(start(df.xts),
-                                                         end(df.xts),
-                                                         "day",
-                                                         drop=F)))[, 1]) %>%
-    tk_tbl() %>%
-    
-    rename("date" = "index", "Value" = "df.xts" )
-}
-#### function to standardize a time-series to a specific start value
-normalization_to_x <- function(dataset, x){
-  #factor_val <- solve(dataset[1],100)
-  #normalized <- dataset * factor_val
-  factor_val <- dataset[1] - x
-  normalize <- dataset - factor_val
-}
-##### dowload economic data, adjust time span, standardize
-current_key <- Sys.getenv("FRED_API_KEY")
-fredr_set_key("c66bdbc4919216612f5cb63ec4994a81")
-
-
-if(econ_data == "dollar_yuan_exch") {
-dollar_yuan_exch <- fredr(
-  series_id = "DEXCHUS",
-  observation_start = start_date,
-  observation_end = end_date,
-  frequency = "d") %>%
-  mutate(value = ifelse(is.na(value) == T, value[is.na(value)+1], value)) %>% 
-  #there is a missing value at 1st of Jan 2019, correct it by setting it to the value of 2nd Jan
-  select(date, value_norm )
-} else if (econ_data == "NASDAQ_CNY"){
-#
-NASDAQ_CNY <- Quandl("NASDAQOMX/NQCN2000CNY",
-                     api_key="jsMbTodosyHDq3sWMuzo",
-                     transform = "normalize",
-                     order = "asc",
-                     collapse = "daily") %>%
-  mutate("date" = .$`Trade Date`, "NASDAQ_Value" = .$`Index Value`) %>% 
-  arrange(date) %>%
-  filter(between(.$date, start_date, end_date)) %>%
-  mutate("NASDAQ_norm" = normalization_to_x(.$NASDAQ_Value, 100)) %>%
-  select(date, NASDAQ_norm )
-} 
+source(str_c(here::here(), "src", "ts_economic_data.R", sep = "/"))
  
+ economic_data(start_date = start_date,
+              end_date = end_date,
+              econ_data = econ_data)
 
 
-##### function to process the articles into a document-token-tibble
-    tidy_text <- function(data) {
-    
-      
-    select(data, id, date, content) %>%
-      mutate(content = str_split(content, "\\|\\|")) %>%
-      unnest(cols = content) %>%
-      group_by(id) %>%
-      mutate(para_id = row_number()) %>%
-      ungroup() %>%
-      tidytext::unnest_tokens(word, content) %>%
-      dplyr::filter(!str_detect(word, "\\d+")) %>% # remove any digit
-      mutate(month = lubridate::month(date, label = TRUE, abbr = FALSE)) %>%
-      select(id, date, month, para_id, word, everything())
-  }
-##### load and process the articles
-article_2020_p_1_td <- read_rds(str_c(here::here(),
-                                      "output",
-                                      "processed_articles_2020_page_01_EN.rds",
-                                      sep = "/")) %>%
-  tidy_text() 
-
-article_2020_p_2_td <- read_rds(str_c(here::here(),
-                                      "output",
-                                      "processed_articles_2020_page_02_EN.rds",
-                                      sep = "/")) %>%
-      tidy_text()
-
-article_2019_p_1_td <- read_rds(str_c(here::here(),
-                                      "output",
-                                      "processed_articles_2019_page_01_EN.rds",
-                                      sep = "/")) %>%
-      tidy_text()
-
-article_2019_p_2_td <- read_rds(str_c(here::here(),
-                                      "output",
-                                      "processed_articles_2019_page_01_EN.rds",
-                                      sep = "/")) %>%
-      tidy_text()
-
-  
-##### check which page should be used for analysis
-  if (page_num == 1) {database = article_2019_p_1_td %>% full_join(article_2020_p_1_td)
-  } else {database = article_2019_p_2_td %>% full_join(article_2020_p_2_td)}
-
+database <- tokenize_aritcles_db()
 
 '#translate the english word into chinese
 transl <- read_rds(str_c(here::here(),
@@ -173,7 +86,7 @@ if (econ_data == "dollar_yuan_exch"){
   } else if (econ_data == "NASDAQ_CNY"){
    tf_idf_NAS <- tf_idf %>%
     mutate(eng_word = normalization_to_x(n, 100)) %>%
-  right_join(NASDAQ_CNY, by = "date")%>%
+  right_join(econ, by = "date")%>%
     select(date, NASDAQ_norm, eng_word ) %>%
     gather(key = "variable", value = "value", -date) %>%
      mutate(value = ifelse(is.na(value) == T, 100, value))
