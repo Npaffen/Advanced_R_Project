e757b92624c1e764ea00ec8446dc9a597e5a1f82
david-rub <david.schulze-r2a@rub.de>
2020-04-10 21:29:29 +0200

    made backup of working app version and cleaned up path

---
+++ Chinese.TM.App_bac/R/scraping/ca_login.R
@@ -0,0 +1,13 @@
+ca_login <- function(username, password){
+
+  remDr <- remoteDriver(remoteServerAddr = "192.168.99.100", port = 4445L)
+
+  remDr$open()
+  remDr$navigate("https://tinyurl.com/rq8vom4")
+
+  remDr$findElement("css selector", "#login :nth-child(1)")$sendKeysToElement(list(as.character(username)))
+  Sys.sleep(3L)
+  remDr$findElement("css selector", "#login :nth-child(2)")$sendKeysToElement(list(as.character(password)))
+  Sys.sleep(3L)
+  remDr$findElement("css selector", "#login_button")$sendKeysToElement(list(key = "enter"))
+}
