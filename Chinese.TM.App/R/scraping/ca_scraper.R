75c6fbf13f2d9d2305cd30771036032b65379264
Npaffen <nils.paffen@wopic.de>
2020-04-14 14:17:58 +0200

    path adjustment for the scraper

--- Chinese.TM.App/R/scraping/ca_scraper.R
+++ Chinese.TM.App/R/scraping/ca_scraper.R
@@ -1,9 +1,9 @@
 
 ca_scraper <- function(username, password, years, month=1:12, paper_length=1:2, personal_2captcha_key){
-  source(str_c(here::here(), "ca_login.R", sep = "/"))
-  source(str_c(here::here(), "ca_articles_url.R", sep = "/"))
-  source(str_c(here::here(), "ca_content.R", sep = "/"))
-  source(str_c(here::here(), "ca_date_urls.R", sep = "/"))
+  source(str_c(here::here(), "R", "scraping",  "ca_login.R", sep = "/"))
+  source(str_c(here::here(), "R", "scraping", "ca_articles_url.R", sep = "/"))
+  source(str_c(here::here(), "R", "scraping", "ca_content.R", sep = "/"))
+  source(str_c(here::here(), "R", "scraping", "ca_date_urls.R", sep = "/"))
 
 
   ca_login(username = username,
